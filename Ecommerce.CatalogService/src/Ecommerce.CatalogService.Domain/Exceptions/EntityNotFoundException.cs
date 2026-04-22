a73811df739955da9982fa697c0b632721ee4570
Vadzim Z. <vadzim_zemchonak@epam.com>
2026-04-22 16:49:08 +0300

    Create solution, Domain layer

---
+++ Ecommerce.CatalogService/src/Ecommerce.CatalogService.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Ecommerce.CatalogService.Domain.Exceptions
+{
+    public class EntityNotFoundException(string entityName, string entityId) 
+        : DomainException($"Entity \"{entityName}\" ({entityId}) was not found.")
+    {
+    }
+}
