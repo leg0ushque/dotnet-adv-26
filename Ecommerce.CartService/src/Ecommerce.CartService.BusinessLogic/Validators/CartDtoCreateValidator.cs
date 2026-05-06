c68f85fbc0d4697f02202f0f4a6c756076752b89
Vadzim Z. <vadzim_zemchonak@epam.com>
2026-05-06 17:12:57 +0300

    Split validator into Create and Update validator - to support different validations for Create/Update

--- Ecommerce.CartService/src/Ecommerce.CartService.BusinessLogic/Validators/CartDtoCreateValidator.cs
+++ Ecommerce.CartService/src/Ecommerce.CartService.BusinessLogic/Validators/CartDtoCreateValidator.cs
@@ -3,18 +3,20 @@ using FluentValidation;
 
 namespace Ecommerce.CartService.BusinessLogic.Validators
 {
-    public class CartDtoValidator : AbstractValidator<CartDto>
+    public class CartDtoCreateValidator : AbstractValidator<CartDto>, ICreateValidator<CartDto>
     {
         public const decimal MinPrice = default;
         public const int MinQuantity = default;
 
-        public CartDtoValidator()
+        public CartDtoCreateValidator()
         {
             RuleFor(x => x.Id)
                 .NotEmpty().WithMessage("Id is required.");
 
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Name is required.");
+
+            /* some logic to verify no existing item with that name exists*/
 
             RuleFor(x => x.Price)
                 .GreaterThan(MinPrice).When(x => x.Price.HasValue)
---
+++ Ecommerce.CartService/src/Ecommerce.CartService.BusinessLogic/Validators/CartDtoUpdateValidator.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.CartService.BusinessLogic.Dtos;
+using FluentValidation;
+
+namespace Ecommerce.CartService.BusinessLogic.Validators
+{
+    public class CartDtoUpdateValidator : AbstractValidator<CartDto>, IUpdateValidator<CartDto>
+    {
+        public const decimal MinPrice = default;
+        public const int MinQuantity = default;
+
+        public CartDtoUpdateValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id is required.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(MinPrice).When(x => x.Price.HasValue)
+                .WithMessage("Price must be non-negative.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThanOrEqualTo(MinQuantity)
+                .NotEmpty().WithMessage("Quantity must be non-negative.");
+        }
+    }
+}
