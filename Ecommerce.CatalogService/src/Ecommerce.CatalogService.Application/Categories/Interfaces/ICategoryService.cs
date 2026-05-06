bf29704357a71604e96a912b4b25c35d4d6efa02
Vadzim Z. <vadzim_zemchonak@epam.com>
2026-05-06 16:22:38 +0300

    Extend Category service - cascade deletion

--- Ecommerce.CatalogService/src/Ecommerce.CatalogService.Application/Categories/Interfaces/ICategoryService.cs
+++ Ecommerce.CatalogService/src/Ecommerce.CatalogService.Application/Categories/Interfaces/ICategoryService.cs
@@ -1,9 +1,12 @@
 using Ecommerce.CatalogService.Application.Categories.DTOs;
 using Ecommerce.CatalogService.Application.Common.Interfaces;
+using Ecommerce.CatalogService.Application.Common.Results;
 
 namespace Ecommerce.CatalogService.Application.Categories.Interfaces
 {
     public interface ICategoryService 
         : IService<CategoryDto, CreateCategoryDto, UpdateCategoryDto>
-    { }
+    {
+        Task<Result> DeleteCategoryWithProductsAsync(string id);
+    }
 }
--- Ecommerce.CatalogService/src/Ecommerce.CatalogService.Application/Categories/Services/CategoryService.cs
+++ Ecommerce.CatalogService/src/Ecommerce.CatalogService.Application/Categories/Services/CategoryService.cs
@@ -3,21 +3,47 @@ using Ecommerce.CatalogService.Application.Categories.DTOs;
 using Ecommerce.CatalogService.Application.Categories.Interfaces;
 using Ecommerce.CatalogService.Application.Common;
 using Ecommerce.CatalogService.Application.Common.Interfaces;
+using Ecommerce.CatalogService.Application.Common.Results;
 using Ecommerce.CatalogService.Domain.Entities;
 using FluentValidation;
 
 namespace Ecommerce.CatalogService.Application.Categories.Services
 {
-    public class CategoryService(IRepository<Category> categoryRepository, 
+    public class CategoryService(IRepository<Category> categoryRepository,
+        IRepository<Product> productRepository,
         IValidator<CreateCategoryDto> createValidator, 
         IValidator<UpdateCategoryDto> updateValidator, 
         IMapper mapper)
         : BaseService<Category, CategoryDto, CreateCategoryDto, UpdateCategoryDto>(categoryRepository, createValidator, updateValidator, mapper), ICategoryService
     {
+        private readonly IRepository<Category> _categoryRepository = categoryRepository;
+        private readonly IRepository<Product> _productRepository = productRepository;
+
+        protected override string EntityName => "Category";
+
         public override void UpdateEntityDetails(Category entityToUpdate, UpdateCategoryDto updateDto) 
             => entityToUpdate.UpdateDetails(
                 updateDto.Name,
                 updateDto.ImageUrl,
                 updateDto.ParentCategoryId);
+
+        public async Task<Result> DeleteCategoryWithProductsAsync(string id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+
+            if (category == null)
+                return Result.Failure(Error.NotFound(EntityName, id));
+
+            var productsInCategory = await _productRepository.GetAllAsync(p => p.CategoryId == id);
+
+            foreach (var product in productsInCategory)
+            {
+                await _productRepository.DeleteByIdAsync(product.Id);
+            }
+
+            await _categoryRepository.DeleteByIdAsync(id);
+
+            return Result.Success();
+        }
     }
 }
