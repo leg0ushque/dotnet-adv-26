a7135ceaf1c75509185c5cf836137b16ddd52430
Vadzim Z. <vadzim_zemchonak@epam.com>
2026-07-17 01:05:35 +0300

    Minor fixes

---
+++ Ecommerce.ApiGateway/src/Ecommerce.ApiGateway.WebApi/Models/ProductDetailsAggregationResponse.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce.ApiGateway.WebApi.Models.Dtos;
+using Ecommerce.ApiGateway.WebApi.Models.GatewayModels;
+
+namespace Ecommerce.ApiGateway.WebApi.Models;
+
+public class ProductDetailsAggregationResponse : AggregationResponseBase
+{
+    public ProductDto? Product { get; set; }
+    public Dictionary<string, string>? Properties { get; set; }
+}
