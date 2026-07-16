9ce50a32e4a8ff93c755effe243e22a8fddd2c34
Vadzim Z. <vadzim_zemchonak@epam.com>
2026-07-17 01:03:26 +0300

    Create common gateway models & Result pattern models

---
+++ Ecommerce.ApiGateway/src/Ecommerce.ApiGateway.WebApi/Models/DownstreamModels/ResourceResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Ecommerce.ApiGateway.WebApi.Models.Downstream;
+
+public class ResourceResponse<T>
+{
+    public T Data { get; set; } = default!;
+    public List<Link> Links { get; set; } = new();
+}
---
+++ Ecommerce.ApiGateway/src/Ecommerce.ApiGateway.WebApi/Models/GatewayModels/AggregationResponseBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Ecommerce.ApiGateway.WebApi.Models.GatewayModels;
+
+public abstract class AggregationResponseBase
+{
+    public AggregateErrorInfo? Error { get; set; }
+}
